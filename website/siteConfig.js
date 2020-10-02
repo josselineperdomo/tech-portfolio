00168adb7f51f86d0dd3216c3426e36042604b5a
Josseline Perdomo <josselineperdomoc@gmail.com>
2020-10-02 15:16:53 -0400

    customized template with my information

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
+    title: 'Josseline Perdomo', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://josselineperdomo.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'josselineperdomo',
+        twitter: 'jossperdomo_',
+        github: 'josselineperdomo',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
